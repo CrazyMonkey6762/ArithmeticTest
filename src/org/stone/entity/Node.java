34db3ab3c4e7756c268ba3167c20bf9a4d109357
FanLaoShi <stone6762@qq.com>
2016-10-22 20:12:26 +0800

    2016年10月22日20:12:05 存储平面上的点的信息

---
+++ src/org/stone/entity/Node.java
@@ -0,0 +1,33 @@
+package org.stone.entity;
+
+/**  
+ * @ClassName_Node
+ * @author_Stone6762  
+ * @CreationTime_2016年10月22日 下午8:07:41
+ * @Description_平面上存储点信息的实体
+ */
+public class Node implements Comparable<Node> {
+		private double x;
+		private double y;
+		
+		public Node(double x, double y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		@Override
+		public int compareTo(Node node) {
+			//按照x排序，x相等时按照y排序
+			double t=this.x-node.x;
+			if (t==0) {
+				return (int)(this.y-node.y);
+			}else {
+				return (int)(t);
+			}
+		}
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y + "]";
+		}
+	
+}
