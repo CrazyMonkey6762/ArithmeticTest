875c05105e7901ab21320a68432ad154ba9e0835
FanLaoShi <stone6762@qq.com>
2016-10-28 17:02:02 +0800

    2016年10月28日17:01:32 动态规划，矩阵连乘问题创建

---
+++ src/org/stone/DynamicProgramming/MatrixMultiplication.java
@@ -0,0 +1,22 @@
+package org.stone.DynamicProgramming;
+/**  
+ * @ClassName_MatrixMultiplication
+ * @author_Stone6762  
+ * @CreationTime_2016年10月28日 下午5:00:18
+ * @Description_矩阵连乘问题
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class MatrixMultiplication {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/org/stone/DynamicProgramming/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Title_package-info.java 
+ * @author_Stone6762  
+ * @CreationTime_2016年10月28日 下午4:52:25
+ * @Description_
+ */
+package org.stone.DynamicProgramming;
